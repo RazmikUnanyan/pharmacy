1583dc442bf30a32e5372db88a6bf9fbc1152679
Razmik <outsource2@omnicampus.io>
2024-06-28 13:35:26 -0500

    ref

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
+
 import "./styles/reset.scss";
 
-ReactDOM.createRoot(document.getElementById('widget-pharmacy')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+declare global {
+    interface Window {
+        renderPharmacyWidget: () => void;
+    }
+}
 
-// declare global {
-//   interface Window {
-//     renderPharmacyWidget: () => void;
-//   }
-// }
-//
-// const renderPharmacyWidget = () => {
-//   ReactDOM.createRoot(document.getElementById("widget-pharmacy")!).render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   );
-// };
-//
-// window.renderPharmacyWidget = renderPharmacyWidget;
+const renderPharmacyWidget = () => {
+    ReactDOM.createRoot(document.getElementById("widget-pharmacy")!).render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    );
+};
+
+window.renderPharmacyWidget = renderPharmacyWidget;
